2ac91f0166e822e905f7d81f218c683b618d7c95
unknown <sonoccity@gmail.com>
2019-12-18 16:12:20 -0800

    Antes de cagarla seguro(ListaPersonajes)

--- src/components/ListaPersonajes.js
+++ src/components/ListaPersonajes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import "./ListaPersonajes.css";
 import { characterInfo } from "../actions/characterAction";
 
 class ListaPersonajes extends React.Component {
@@ -8,24 +9,38 @@ class ListaPersonajes extends React.Component {
     await this.props.characterInfo();
   }
 
+  sortProps() {
+    const { characters } = this.props;
+    return characters
+      ? characters.sort(function(a, b) {
+          if (a.id < b.id) {
+            return -1;
+          }
+          if (a.id > b.id) {
+            return 1;
+          }
+          return 0;
+        })
+      : null;
+  }
   renderCard() {
     const { characters } = this.props;
     return characters
-      ? characters.map(info => {
+      ? characters.map((info, index) => {
           return (
-            <div id={info.id}>
-              <div class="col s1 m2">
-                <div class="card">
-                  <div class="card-image">
-                    <img src={info.url} />
+            <div id={info.id} key={index}>
+              <div className="col s1 m2 ">
+                <div className="card">
+                  <div className="card-image">
+                    <img src={info.url} alt="Pelaste" className="height" />
                     <Link
                       to="/characters/{characterId}/rates"
-                      class="btn-floating halfway-fab waves-effect waves-light red"
+                      className="btn-floating halfway-fab waves-effect waves-light red"
                     >
-                      <i class="material-icons">arrow_forward</i>
+                      <i className="material-icons">arrow_forward</i>
                     </Link>
                   </div>
-                  <div class="card-content">
+                  <div className="card-content">
                     <p>{info.id}</p>
                   </div>
                 </div>
@@ -40,10 +55,10 @@ class ListaPersonajes extends React.Component {
     return (
       <div className="container">
         {/* Carta del personaje */}
-        <div class="row">{this.renderCard()}</div>
+        <div className="row">{(this.sortProps(), this.renderCard())}</div>
 
         {/* Boton para ir a agregar personaje */}
-        <Link to="/addCharacter" class="waves-effect waves-light btn">
+        <Link to="/addCharacter" className="waves-effect waves-light btn">
           Agregar Personaje
         </Link>
       </div>
